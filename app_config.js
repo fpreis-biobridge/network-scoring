69e13dcb379443bee134966e71cb0ee32bb8aadb
fpreis-biobridge <fpreis@biobridgepartners.com>
2026-03-20 18:54:42 +0100

    Add files via upload

---
+++ app_config.js
@@ -0,0 +1,5 @@
+const APP_CONFIG = {
+  AZURE_CLIENT_ID: "7ac2cfb3-b30e-4719-addc-cb17ee41a4a9",
+  AZURE_TENANT_ID: "0493c27b-2b9b-428b-a532-b07cc4a28d9d",
+  SHAREPOINT_SITE:  "https://biobridgepartners.sharepoint.com/sites/BiobridgeNetworkMap",
+};
