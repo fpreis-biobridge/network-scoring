7aac3b40b5fbda0653725d02522ec8bf15a861b9
biobridge-partners <fpreis@biobridgepartners.com>
2026-04-07 16:02:00 +0200

    Delete app_config.js

--- app_config.js
+++
@@ -1,5 +0,0 @@
-var APP_CONFIG = {
-  AZURE_CLIENT_ID: "7ac2cfb3-b30e-4719-addc-cb17ee41a4a9",
-  AZURE_TENANT_ID: "c6df2716-3819-4281-a465-7b12a8e922a7",
-  SHAREPOINT_SITE:  "https://biobridgepartners.sharepoint.com/sites/BiobridgeNetworkMap",
-};
